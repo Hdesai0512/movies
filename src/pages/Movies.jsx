30c2200538d49f40495df794303715d397fcd8f7
Hdesai97 <hdesai97@gmail.com>
2023-05-07 04:06:06 -0400

    changes made

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { fetchMovies, getGenres } from '../store';
+import { onAuthStateChanged } from 'firebase/auth';
+import { firebaseAuth } from '../utils/Firebase-config';
+import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
+import styled from 'styled-components';
+import NotAvailable from '../components/NotAvailable';
 export default function Movies() {
     const [isScrolled, setIsScrolled] = useState(false);
     const navigate = useNavigate();
@@ -14,16 +20,46 @@ export default function Movies() {
     },[]);
 
     useEffect(() =>{
-        if(genresLoaded) dispatch(fetchMovies({ type: "movies"}));
-    })
+        if(genresLoaded) {dispatch(fetchMovies({ type: "movie"}));
+    }
+}, [genresLoaded]);
 
-  window.onscroll = () => {
+const [user, setUser] = useState(undefined);
+
+onAuthStateChanged(firebaseAuth, (currentUser)=>{
+   if (currentUser) setUser(currentUser.uid);
+   else navigate("/login");
+});
+window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
  
 };
 
-  return (
-    <div>Movies</div>
-  )
+  return  <Container>
+   <div className="navbar">
+        <Navbar isScrolled={isScrolled} />
+   </div>
+        <div className="data">
+            {
+                movies.length ? <Slider movies={movies} /> :
+                <NotAvailable />
+            }
+        </div>
+        </Container>
+  
 }
+
+const Container = styled.div`
+
+.data{
+    margin-top: 8rem;
+    .not-available {
+        text-align: center;
+        color: white;
+        margin-top:4rem;
+    }
+}
+
+
+`
